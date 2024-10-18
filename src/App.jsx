bdba9132ba4d05a577750ce4c92be997aaab5783
Dunsin <58459950+Dunsino@users.noreply.github.com>
2024-10-18 17:47:06 +0100

    Hero section update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden bg-white2">
-      <Navbar />
+      <div className="relative overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
   );
 };
